f2241265c84d02a72d7bdff40629c30458b72f46
Pedro Ribeiro <pedro.ribeiro@york.ac.uk>
2020-12-18 11:27:19 +0000

    Updated wercker setup for new host.

--- upload.sh
+++ upload.sh
@@ -1,19 +1,37 @@
 echo "Get current version"
 dir=circus.robocalc.robosim.update/target/repository/
-remote=/shared/storage/cs/www/robostar/robotool/robosim-metamodel/
-url=ahm504@sftp.york.ac.uk
+remote=${ROBOSTAR_WEB_ROOT}/robotool/robosim-metamodel/
+url=${ROBOSTAR_WEB_USER}@${ROBOSTAR_WEB_HOST}
 file=$(ls $dir/features | grep -m 1 jar)
 version=${file#*_}
 version=${version%.jar}
+
+# Use the branch name to choose the name of the branch. This assumes
+# no branch of name 'update' will ever be used.
+if [[ $WERCKER_GIT_BRANCH = master ]];
+then
+  update=update
+else
+  update=$WERCKER_GIT_BRANCH
+fi
+
 if [[ $version = *[!\ ]* ]]; 
 then 
   echo "Current version:" $version;
+  echo "Branch:" $WERCKER_GIT_BRANCH;
+  dest=${update}_${version}
+  echo "Target dir:" $dest;
   rm -rf tmp
   mkdir tmp && cd tmp
-  dest=update_$version
   mkdir $dest
   cp -r ../$dir/* $dest
-  ln -s $dest update
+
+  # In the new host, it is not possible to generate a symlink that points to
+  # a non-existent target, such as 'update', before it is actually created.
+  # So here we first transfer the update folder, then create the symlink and
+  # finally transfer that too.
+  rsync -a -e "ssh" -rtzh . $url:$remote
+  ln -s $dest ${update}
   rsync -a -e "ssh" -rtzh . $url:$remote
   exit $?;
 else
